dcfaf054f80dd0bf46db66bdded8ec0072d3fd41
Farzana <id.farzankhan@gmail.com>
2023-10-08 09:14:18 +0500

    legal and illegal name

---
+++ index.js
@@ -0,0 +1,32 @@
+// 1. Declare 3 variables in one statement.
+For example: let = $name, _name or name
+// 2. Declare 5 legal & 5 illegal variable names.
+// 5 Legal names 
+let userName ="xyz";
+let user_Info ="xyz";
+let _userData ="xyz"
+let num_12 = 23;
+let $sum= 45
+
+//  5 illegal variable names
+let #names ="xyz";
+let 6data =88;
+let Bio-Data = "ghgjghj@gmail";
+var first&Named ="name"
+let contact-Data = 56755576576
+
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+ b-answer) $ ,number and _score Example : $name , _name , name.
+
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. 
+For example $name, _name or name
+// d) Variable names are case _________ 
+sensitive
+// e) Variable names should not be JS _________ 
+keywords
